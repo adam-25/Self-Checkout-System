315015dc1f3e2059a46b627caf537f3fa347075d
Simon Vincent <47257674+SmonVincent@users.noreply.github.com>
2022-04-04 13:57:49 -0600

    refillCoinDispenser Completed
    Completed the refillCoinDispenser method. Is yet to be tested.

--- SCS - Software/src/seng300/software/AttendantLogic.java
+++ SCS - Software/src/seng300/software/AttendantLogic.java
@@ -1,11 +1,18 @@
 package seng300.software;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import org.lsmr.selfcheckout.Coin;
+import org.lsmr.selfcheckout.SimulationException;
+import org.lsmr.selfcheckout.devices.OverloadException;
 import org.lsmr.selfcheckout.devices.SelfCheckoutStation;
 import org.lsmr.selfcheckout.devices.SupervisionStation;
 
 public class AttendantLogic {
 
 	private SupervisionStation ss;
+	private boolean loggedIn;
 	
 	public AttendantLogic(SupervisionStation supervisionStation)
 	{
@@ -22,15 +29,27 @@ public class AttendantLogic {
 		
 	}
 	
-	public void refillsCoinDispenser(SelfCheckoutStation sc)
+	public void refillsCoinDispenser(SelfCheckoutStation sc) throws SimulationException, OverloadException
 	{
-		
+	
+	if (loggedIn) {
+		for (int i = 0; i <= sc.coinDenominations.size(); i++) {
+			
+		int loadedCoins = sc.coinDispensers.get(sc.coinDenominations.get(i)).size();
+		int dispenserCapacity = sc.coinDispensers.get(sc.coinDenominations.get(i)).getCapacity();
+		int coinsToAdd = dispenserCapacity - loadedCoins;
+			
+		for (int j = 0; j <= coinsToAdd; i++) {	
+			sc.coinDispensers.get(sc.coinDenominations.get(i)).load(new Coin(Currency.getInstance("CAD"), sc.coinDenominations.get(i)));
+			}
+		}
+	}
 	}
 	
 	public void refillsBanknoteDispenser(SelfCheckoutStation sc)
 	{
 		
 	}
-	
+	//MAKE LOGIN METHOD
 	
 }
