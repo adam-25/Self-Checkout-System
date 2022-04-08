8ad27a9be346a5f5d5e182ed22d7cbb232ff4749
Makeda Morris <hmakeda71@gmail.com>
2022-04-08 09:43:34 -0600

    more improvements

--- SCS - Software/src/seng300/software/GUI/BaggingAreaPanel.java
+++ SCS - Software/src/seng300/software/GUI/BaggingAreaPanel.java
@@ -1,3 +1,5 @@
+package seng300.software.GUI;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,22 +11,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-
+import java.awt.EventQueue;
 import java.util.ArrayList;
+import java.awt.GridLayout;
+import javax.swing.border.EmptyBorder;
+import java.awt.Insets;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
-public class BaggingAreaPanel extends JFrame implements ActionListener {
-    JButton returnButton;
-    JButton deleteButton;
-    JButton upButton;
-    JButton downButton;
+public class BaggingAreaPanel extends JPanel implements ActionListener
+{
+    public final JButton returnButton;
+    public final JButton deleteButton;
+    
+    private JButton upButton;
+    private JButton downButton;
 
-    JLabel windowText;
-    JLabel highText;
-    JLabel middleText;
-    JLabel lowText;
+    private JLabel windowText;
+    private JLabel highText;
+    private JLabel middleText;
+    private JLabel lowText;
 
-    int n = 0;
-    int arraySize;
+    private int n = 0;
+    private int arraySize;
 
     ArrayList<String> cars;
 
@@ -43,36 +52,55 @@ public class BaggingAreaPanel extends JFrame implements ActionListener {
             if(n == (arraySize-1)){
                 lowText.setText("");
             }else{
-                highText.setText(stuff.get(n+1));
+                lowText.setText(stuff.get(n+1));
             }
         }
     }
 
-    BaggingAreaPanel(ArrayList<String> carsInput) {
+    public BaggingAreaPanel(ArrayList<String> carsInput)
+    {
+    	setBorder(new EmptyBorder(15, 15, 15, 15));
         cars = carsInput;
-
         arraySize = cars.size();
 
         //----Setting up Text----//
         windowText = new JLabel();
+        windowText.setHorizontalAlignment(SwingConstants.CENTER);
+        windowText.setFont(new Font("Tahoma", Font.BOLD, 32));
         windowText.setText("Bagging Area");
         windowText.setBounds(0, 0, 100, 100);
 
         highText = new JLabel();
-        highText.setText("");
+        highText.setOpaque(true);
+        highText.setHorizontalAlignment(SwingConstants.CENTER);
+        highText.setFont(new Font("Tahoma", Font.BOLD, 28));
+        highText.setForeground(new Color(230, 230, 250));
+        highText.setBackground(new Color(0, 0, 128));
         highText.setBounds(0, 100, 100, 100);
 
         middleText = new JLabel();
+        middleText.setOpaque(true);
+        middleText.setHorizontalAlignment(SwingConstants.CENTER);
+        middleText.setFont(new Font("Tahoma", Font.BOLD, 42));
+        middleText.setForeground(new Color(230, 230, 250));
+        middleText.setBackground(new Color(0, 0, 128));
         middleText.setText("");
         middleText.setBounds(0, 50, 100, 100);
 
         lowText = new JLabel();
+        lowText.setOpaque(true);
+        lowText.setHorizontalAlignment(SwingConstants.CENTER);
+        lowText.setFont(new Font("Tahoma", Font.BOLD, 28));
+        lowText.setForeground(new Color(230, 230, 250));
+        lowText.setBackground(new Color(0, 0, 128));
         lowText.setText("");
         lowText.setBounds(0, 0, 100, 100);
         //----Setting up Text----//
 
         //----Setting up Buttons----//
         returnButton = new JButton();
+        returnButton.setBackground(new Color(240, 248, 255));
+        returnButton.setMargin(new Insets(2, 14, 2, 0));
         returnButton.setBounds(0, 0, 100, 100);
         returnButton.addActionListener(this);
         returnButton.setText("RETURN");
@@ -80,6 +108,7 @@ public class BaggingAreaPanel extends JFrame implements ActionListener {
         returnButton.setBorder(BorderFactory.createEtchedBorder());
 
         deleteButton = new JButton();
+        deleteButton.setBackground(new Color(255, 228, 225));
         deleteButton.setBounds(100, 0, 100, 100);
         deleteButton.addActionListener(this);
         deleteButton.setText("DELETE");
@@ -87,6 +116,7 @@ public class BaggingAreaPanel extends JFrame implements ActionListener {
         deleteButton.setBorder(BorderFactory.createEtchedBorder());
 
         upButton = new JButton();
+        upButton.setBackground(new Color(240, 248, 255));
         upButton.setBounds(0, 0, 100, 100);
         upButton.addActionListener(this);
         upButton.setText("UP");
@@ -94,6 +124,7 @@ public class BaggingAreaPanel extends JFrame implements ActionListener {
         upButton.setBorder(BorderFactory.createEtchedBorder());
 
         downButton = new JButton();
+        downButton.setBackground(new Color(240, 248, 255));
         downButton.setBounds(100, 0, 100, 100);
         downButton.addActionListener(this);
         downButton.setText("DOWN");
@@ -102,18 +133,21 @@ public class BaggingAreaPanel extends JFrame implements ActionListener {
         //----Setting up Buttons----//
 
         //----Setting up Frame----//
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setSize(500, 500);
-        this.setLayout(new BorderLayout(10,10));
-        this.setVisible(true);
+//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        getContentPane().setLayout(null);
+//        this.setSize(952, 707);
+//        getContentPane().setLayout(new BorderLayout(10,10));
+//        this.setVisible(true);
         //----Setting up Frame----//
 
         //----Setting up Panels----//
         JPanel panel1 = new JPanel();
         JPanel panel2 = new JPanel();
+        panel2.setBorder(new EmptyBorder(5, 5, 5, 5));
         JPanel panel3 = new JPanel();
+        panel3.setBorder(new EmptyBorder(5, 5, 5, 5));
         JPanel panel4 = new JPanel();
+        panel4.setBorder(new EmptyBorder(5, 50, 5, 50));
 
         panel1.setBackground(Color.red);
         panel2.setBackground(Color.green);
@@ -124,22 +158,36 @@ public class BaggingAreaPanel extends JFrame implements ActionListener {
         panel2.setPreferredSize(new Dimension(75,100));
         panel3.setPreferredSize(new Dimension(100,75));
         panel4.setPreferredSize(new Dimension(100,100));
+        setLayout(new BorderLayout(0, 0));
         
-        this.add(panel1,BorderLayout.NORTH);
-        this.add(panel2,BorderLayout.EAST);
-        this.add(panel3,BorderLayout.SOUTH);
-        this.add(panel4,BorderLayout.CENTER);
+//        getContentPane().add(panel1,BorderLayout.NORTH);
+//        getContentPane().add(panel2,BorderLayout.EAST);
+//        getContentPane().add(panel3,BorderLayout.SOUTH);
+//        getContentPane().add(panel4,BorderLayout.CENTER);
+        
+        // -- Content Pane (JPanel)
+        //     -- content
+        
+        add(panel1, BorderLayout.NORTH);
+        add(panel2, BorderLayout.EAST);
+        add(panel3, BorderLayout.SOUTH);
+        add(panel4, BorderLayout.CENTER);
+        
+        panel1.setLayout(new GridLayout(0, 1, 0, 0));
         //----Setting up Panels----//
 
         panel1.add(windowText);
+        panel4.setLayout(new GridLayout(3, 1, 0, 10));
         panel4.add(highText);
         panel4.add(middleText);
         panel4.add(lowText);
 
         displayOfItems(cars);
+        panel3.setLayout(new GridLayout(1, 2, 5, 0));
 
         panel3.add(returnButton);
         panel3.add(deleteButton);
+        panel2.setLayout(new GridLayout(0, 1, 0, 5));
 
         panel2.add(upButton);
         panel2.add(downButton);
@@ -155,14 +203,37 @@ public class BaggingAreaPanel extends JFrame implements ActionListener {
             System.out.println("Delete Button clicked");
             displayOfItems(cars);
         }else if(e.getSource()==upButton){
-            if(n < arraySize -1) n += 1;
+        	if(n > 0) n -= 1;
             System.out.println("Up Button clicked " + n);
             displayOfItems(cars);
         }else if(e.getSource()==downButton){
-            if(n > 0) n -= 1;
+        	if(n < arraySize -1) n += 1;
             System.out.println("Down Button clicked " + n);
             displayOfItems(cars);
         }
 
     }
+    
+    /**
+	 * Launch the application. TO BE USED FOR TESTING ONLY!
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ArrayList<String> test = new ArrayList<>();
+					test.add("hi");
+					test.add("bye");
+					test.add("nope");
+					test.add("goat");
+					JFrame frame = new JFrame();
+					frame.getContentPane().add(new BaggingAreaPanel(test));
+					frame.pack();
+					frame.setVisible(true);					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
