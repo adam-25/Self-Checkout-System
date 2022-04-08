1330763e6033d967bccb35437da522fa43624195
Muhammad Ali <muhammad.ali4@ucalgary.ca>
2022-04-08 17:57:09 -0600

    removed invalid import

--- SCS - Software/src/seng300/software/GUI/PinPad.java
+++ SCS - Software/src/seng300/software/GUI/PinPad.java
@@ -6,7 +6,6 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import net.miginfocom.swing.MigLayout;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionListener;
