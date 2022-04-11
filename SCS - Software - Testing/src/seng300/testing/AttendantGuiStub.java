027140552c0aa03238b39cfb4667f4e682b565bd
Ibrahim Sultan <sultanibrahim01@gmail.com>
2022-04-11 17:00:26 -0600

    TestFixing
    Iteration 1

---
+++ SCS - Software - Testing/src/seng300/testing/AttendantGuiStub.java
@@ -0,0 +1,40 @@
+package seng300.testing;
+
+import seng300.software.SelfCheckoutSystemLogic;
+import seng300.software.GUI.BlockNotifiableGui;
+
+public class AttendantGuiStub implements BlockNotifiableGui{
+
+	@Override
+	public void notifyOwnBagBlock(SelfCheckoutSystemLogic stationOfConcern) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyWeightDiscBlock(SelfCheckoutSystemLogic stationOfConcern) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyRemoveProductBlock(SelfCheckoutSystemLogic selfCheckoutSystemLogic) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyPrinterOutPaper(SelfCheckoutSystemLogic selfCheckoutSystemLogic) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyPrinterOutInk(SelfCheckoutSystemLogic selfCheckoutSystemLogic) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
